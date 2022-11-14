29fc43ad992409c5be2386b781979fddbeed5ea4
subhadip1203 <subhadip1203@gmail.com>
2022-11-13 23:11:21 -0800

    tupple typeScript

---
+++ basic/tuples .ts
@@ -0,0 +1,5 @@
+
+// array with fixed elements
+// although we can push data into tupple , which is bit odd
+const tpl : [number, string] = [10, 'subhadip']
+// tpl[1]=20  // this will be invalid as index 1 will always be string not number
